6b933f9d81d66b15a6c999d8fde7ca49c7191e69
Olena Koptylo <akp2315@gmail.com>
2019-01-19 16:49:16 -0500

    third commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,7 @@ public class Calculator {
     public static double subtract(double num1, double num2){
         return num1 - num2;
     }
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
 }
